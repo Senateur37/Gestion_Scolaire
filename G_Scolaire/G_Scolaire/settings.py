f2c03477ec3b46cd64e3c2d8462ac8677455db10
senateur37 <mahamadousacko599@gmail.com>
2026-08-27 13:09:51 +0000

    Fix DATABASE_URL parsing fallback in settings.py

--- G_Scolaire/G_Scolaire/settings.py
+++ G_Scolaire/G_Scolaire/settings.py
@@ -104,13 +104,22 @@ WSGI_APPLICATION = 'G_Scolaire.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
-}
+db_url = os.environ.get('DATABASE_URL', '').strip()
+if db_url and db_url.startswith(('postgres://', 'postgresql://', 'sqlite://', 'mysql://')):
+    DATABASES = {
+        'default': dj_database_url.parse(
+            db_url,
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
